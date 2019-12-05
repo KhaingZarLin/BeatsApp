e58d582c3e98d54bd533cc9a081d3f3a0669640b
Khaing Zar Lin <khaingzarlin182@gmail.com>
2019-12-05 22:17:17 +0630

    initial commit

---
+++ app/src/main/java/com/example/beatsellingapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.beatsellingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnlogin.setOnClickListener(View.OnClickListener {
+                val intent= Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+        })
+    }
+}
